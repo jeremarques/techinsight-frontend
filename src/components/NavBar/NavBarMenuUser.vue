8d30289942444cb7f297740c4e492fba7a2c942d
Jeremias Marques <saimerejmarques@gmail.com>
2024-01-31 17:35:21 -0300

    Feature: Add new user dropdown menu in navbar, and new variant to button

--- src/components/NavBar/NavBarMenuUser.vue
+++ src/components/NavBar/NavBarMenuUser.vue
@@ -1,68 +1,60 @@
 <template>
-<Sheet>
-    <SheetTrigger as-child>
-        <button type="button" class="flex items-center focus:outline-none md:pl-8" aria-label="toggle profile dropdown">
-            <Avatar class="border border-gray-400 dark:border-gray-500 w-9 h-9 md:w-10 md:h-10">
-                <AvatarImage v-if="!!userData.profile_photo" :src="userData.profile_photo" alt="imagem do usuário"/>
-                <AvatarImage v-if="!!!userData.profile_photo" src="../../assets/images/user-profile.png" alt="imagem do usuário"/>
-                <AvatarFallback>{{ userData.user.username }}</AvatarFallback>
-            </Avatar>
-        </button>
-    </SheetTrigger>
-    <SheetContent>
-        <h2 class="font-regular text-sm -mt-2 text-gray-600 dark:text-gray-300">Menu do usuário</h2>
-
-        <div class="mt-12 font-regular text-sm">
-            <div class="flex flex-col items-center gap-x-4 ">
-                <Avatar class="border-2 border-gray-300 dark:border-gray-400 h-20 w-20" size="lg">
+    <DropdownMenu>
+        <DropdownMenuTrigger class="md:pl-8 flex" as-child>
+            <button type="button" class="flex items-center focus:outline-none" aria-label="toggle profile dropdown">
+                <Avatar class="border border-gray-400 dark:border-gray-500 size-9 md:size-10">
                     <AvatarImage v-if="!!userData.profile_photo" :src="userData.profile_photo" alt="imagem do usuário"/>
-                    <AvatarImage v-else src="../../assets/images/user-profile.png" alt="imagem do usuário"/>
+                    <AvatarImage v-if="!!!userData.profile_photo" src="../../assets/images/user-profile.png" alt="imagem do usuário"/>
                     <AvatarFallback>{{ userData.user.username }}</AvatarFallback>
                 </Avatar>
-                <span class="text-base font-medium mt-4 mb-6">{{ userData.user.full_name }}</span>
-            </div>
-
-            <div class="group relative flex items-center p-1 gap-x-4 rounded-lg hover:bg-gray-50 dark:hover:bg-dark-mixed-200">
-                <div class="p-2 rounded-lg flex items-center justify-center bg-gray-50 group-hover:bg-white dark:bg-dark-mixed-200/70 dark:group-hover:bg-dark-mixed-300">
-                    <CircleUserRound :stroke-width="1.35" />
+            </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent class="mt-1 mr-4 xl:mr-8 2xl:mr-20 w-[220px]">
+            <DropdownMenuLabel class="flex flex-col">
+                <span class="font-medium text-sm">Jeremias Marques</span>
+                <small class="text-gray-500 font-regular">jeremarques</small>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+                <div class="flex items-center gap-3">
+                    <User :stroke-width="1.50" class="size-6" />
+                    <a href="#" class="font-regular text-gray-900 dark:text-white">
+                        Perfil
+                    </a>
                 </div>
-                <a href="#" class="text-gray-900 dark:text-white">
-                    Perfil
-                </a>
-            </div>
-            <div class="group relative flex items-center p-1 gap-x-4 rounded-lg hover:bg-gray-50 dark:hover:bg-dark-mixed-200">
-                <div class="p-2 rounded-lg flex items-center justify-center bg-gray-50 group-hover:bg-white dark:bg-dark-mixed-200/70 dark:group-hover:bg-dark-mixed-300">
-                    <FileText :stroke-width="1.35" />
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <div class="flex items-center gap-3">
+                    <FileText :stroke-width="1.30" class="size-6" />
+                    <a href="#" class="font-regular text-gray-900 dark:text-white">
+                        Seus Insights
+                    </a>
                 </div>
-                <a href="#" class="text-gray-900 dark:text-white">
-                    Seus insights
-                </a>
-            </div>
-            <div class="group relative flex items-center p-1 gap-x-4 rounded-lg hover:bg-gray-50 dark:hover:bg-dark-mixed-200">
-                <div class="p-2 rounded-lg flex items-center justify-center bg-gray-50 group-hover:bg-white dark:bg-dark-mixed-200/70 dark:group-hover:bg-dark-mixed-300">
-                    <Settings :stroke-width="1.35" />
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <div class="flex items-center gap-3">
+                    <Settings :stroke-width="1.40" class="size-6" />
+                    <a href="#" class="font-regular text-gray-900 dark:text-white">
+                        Configurações
+                    </a>
                 </div>
-                <a href="#" class="text-gray-900 dark:text-white">
-                    Configurações
-                </a>
-            </div>
-            <div class="group relative flex items-center p-1 gap-x-4 rounded-lg hover:bg-gray-50 dark:hover:bg-dark-mixed-200">
-                <div class="p-2 rounded-lg flex items-center justify-center bg-gray-50 group-hover:bg-white dark:bg-dark-mixed-200/70 dark:group-hover:bg-dark-mixed-300">
-                    <HelpCircle :stroke-width="1.35" />
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <div class="flex items-center gap-3">
+                    <HelpCircle :stroke-width="1.40" class="size-6" />
+                    <a href="#" class="font-regular text-gray-900 dark:text-white">
+                        Ajuda
+                    </a>
                 </div>
-                <a href="#" class="font-regular text-gray-900 dark:text-white">
-                    Ajuda
-                </a>
-            </div>
-            <div class="mt-4">
-                <Button variant="outline" class="group transition-all duration-400 border-none font-regular dark:bg-transparent dark:border-dark-mixed-200 hover:border-none hover:bg-transparent p-2 hover:ml-1">
-                    <span class="transition-color duration-200 group-hover:text-red-500 mr-2">Sair</span>
-                    <LogOut :stroke-width="2" :size="14" class="transition-color duration-200 group-hover:text-red-500" />
+            </DropdownMenuItem>
+            <div class="px-2 py-2">
+                <Button variant="destructiveOutilined" size="sm" class="gap-2 w-full">
+                    <span>Sair</span>
+                    <LogOut :stroke-width="2" class="size-4" />
                 </Button>
             </div>
-        </div>
-    </SheetContent>
-</Sheet>
+        </DropdownMenuContent>
+    </DropdownMenu>
 </template>
 
 <script setup>
@@ -78,8 +70,19 @@ import {
     FileText,
     Settings,
     HelpCircle,
+    UserRound,
+    User,
     LogOut
 } from 'lucide-vue-next'
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 
 const props = defineProps({
     userData: {
